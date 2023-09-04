74679cdecbaeef31d8f4067e523d178b5c5fb045
RomaChumak <romachumak355@gmail.com>
2023-09-04 14:08:02 +0300

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,22 @@ export class App extends Component {
             { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
         ],
         filter: '',
-    };
+  };
+  
+componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const jsonParse = JSON.parse(contacts);
+    if (jsonParse) {
+      this.setState({ contacts: jsonParse });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
     searchContact = (contactName) => {
         this.setState({
             filter: contactName,
